4750b1962e19863ac7c547fc54c965d156074b89
Game <nghoming72@gmail.com>
2018-04-01 22:33:00 -0700

    update

--- client/src/component/SignUp/SignUp.js
+++ client/src/component/SignUp/SignUp.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+handeleSignUp = event => {
+    // <Router>
+    //     <div>
+    //         <Switch>
+    //             <Route exact path="/" component={Landing} />
+    //             <Route exact path="/profile" component={Profile} />
+    //         </Switch>
+    //     </div>
+    // </Router>;
+}
 
 const Signup = props => (
-
     <div className='signup-button'>
-        <button onClick={prop.handleClick}>Sign Up</button>
+        <button onClick={prop.handeleSignUp}>Sign Up</button>
     </div>
 )
 
