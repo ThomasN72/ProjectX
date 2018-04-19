bc9dd90a70049456c3066246e9b5c83946b8569f
Christina <cmatson93@gmail.com>
2018-04-19 13:46:06 -0700

    changed .gitignore

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-// const db = require("./models");
 const routes = require("./routes");
 
 
@@ -19,27 +18,11 @@ app.use(express.static("client/build"));
 app.use(routes);
 
 const MONGODB_URI = "mongodb://heroku_lkxldss7:a16oev3efogqsamras216lfrr0@ds149309.mlab.com:49309/heroku_lkxldss7";
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
-// mongoose.Promise = Promise;
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/oneappDB";
-// mongoose.connect(MONGODB_URI);
-
-// if (process.env.MONGODB_URI) {
-//     mongoose.connect(MONGODB_URI, {
-//         useMongoClient: true
-//     })
-// } else {
-//     mongoose.connect("mongodb://localhost/oneappDB");
-// };
-
-// Set up promises with mongoose
-// mongoose.Promise = global.Promise;
 
 
 // Connect to the Mongo DB
 mongoose.connect(
-    process.env.MONGODB_URI
+    process.env.MONGODB_URI || "mongodb://localhost/oneappDB"
 );
 
 app.listen(PORT, function() {
